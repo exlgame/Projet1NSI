748f5adf1916180053df62f59f6025df52ce79d7
exlgame <164381172+exlgame@users.noreply.github.com>
2024-10-15 19:20:42 +0200

    suivie tuto

--- code/game.py
+++ code/game.py
@@ -1,6 +1,13 @@
+import pygame
+from screen import Screen
+
+
 class Game:
+
     def __int__(self):
         self.running = True
+        self.screen = Screen()
+
     def run(self):
         while self.running:
             pass
