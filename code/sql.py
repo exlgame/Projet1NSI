c33913ec4e3da03848f8221a96ed4042b101db18
exlgame <164381172+exlgame@users.noreply.github.com>
2024-10-18 16:46:12 +0200

    Adding sql

--- code/sql.py
+++ code/sql.py
@@ -1,3 +1,15 @@
 import sqlite3
 
-sqlite3.connect("../assets/json/base.db")
+class SQL:
+    def __init__(self):
+        self.connection = sqlite3.connect('..//assets/json/base.db')
+        self.cursor = self.connection.cursor()
+
+    def get_connection(self):
+        return self.connection
+
+    def get_cursor(self):
+        return self.cursor
+
+    def select(self,table:str, columns:list[str], where:str= None,order: str = None,limit:int=None) -> list[tuple]:
+        query:str = f"SELECT"
